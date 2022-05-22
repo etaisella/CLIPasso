ff6868fd11298e07386781e8efdb24aeadc902db
etaisella <74829220+etaisella@users.noreply.github.com>
2022-05-22 21:54:47 +0300

    Update painterly_rendering.py

--- painterly_rendering.py
+++ painterly_rendering.py
@@ -32,8 +32,8 @@ def load_renderer(args, target_im=None, mask=None):
                        imsize=args.image_scale,
                        device=args.device,
                        target_im=target_im,
-                       mask=mask
-                       pixelArt = args.pixelArt)
+                       pixelArt=args.pixelArt,
+                       mask=mask)
     renderer = renderer.to(args.device)
     return renderer
 
