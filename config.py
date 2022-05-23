b77bee7101376cef0df36d9aae434365e78a1285
etaisella <74829220+etaisella@users.noreply.github.com>
2022-05-23 12:12:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ def parse_arguments():
     parser.add_argument("--mask_object", type=int, default=0)
     parser.add_argument("--fix_scale", type=int, default=0)
     parser.add_argument("--display_logs", type=int, default=0)
-    parser.add_argument("--pixelArt", type=int, default=0)
     parser.add_argument("--display", type=int, default=0)
+    parser.add_argument("--pixelArt", type=int, default=0)
 
     # =================================
     # ============ wandb ============
@@ -85,8 +85,8 @@ def parse_arguments():
                         help="the type of perceptual loss to be used (L2/LPIPS/none)")
     parser.add_argument("--perceptual_weight", type=float, default=0,
                         help="weight the perceptual loss")
-    parser.add_argument("--train_with_clip", type=int, default=1)
-    parser.add_argument("--clip_weight", type=float, default=0.05)
+    parser.add_argument("--train_with_clip", type=int, default=0)
+    parser.add_argument("--clip_weight", type=float, default=0)
     parser.add_argument("--start_clip", type=int, default=0)
     parser.add_argument("--num_aug_clip", type=int, default=4)
     parser.add_argument("--include_target_in_aug", type=int, default=0)
