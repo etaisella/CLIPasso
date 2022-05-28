d3b6059dd145ecb9559369a4a8d5b39aeb6a1149
etaisella <74829220+etaisella@users.noreply.github.com>
2022-05-28 21:02:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,16 @@ def parse_arguments():
     parser.add_argument("--fix_scale", type=int, default=0)
     parser.add_argument("--display_logs", type=int, default=0)
     parser.add_argument("--display", type=int, default=0)
+    
+    # =================================
+    # =========== PixelArt ============
+    # =================================
     parser.add_argument("--pixelArt", type=int, default=1)
+    parser.add_argument("--canvasW", type=int, default=32)
+    parser.add_argument("--canvasH", type=int, default=32)
+    parser.add_argument("--pixelArt", type=int, default=1)
+    parser.add_argument("--quantizeColors", type=int, default=1)
+    parser.add_argument("--numColors", type=int, default=1024)
 
     # =================================
     # ============ wandb ============
