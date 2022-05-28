68a62c103709b4bad5e79869a8897ea97dc7bb9e
etaisella <74829220+etaisella@users.noreply.github.com>
2022-05-28 17:50:28 +0300

    Update painter_params.py

--- models/painter_params.py
+++ models/painter_params.py
@@ -88,6 +88,7 @@ class Painter(torch.nn.Module):
         print("Setting PA canvas in class init")
         N, C, H, W = 1, 3, self.canvas_height, self.canvas_width
         rand_idxs = torch.randint(low=0, high=self.num_colors -1, size=(H, W))
+        print(rand_idxs)
         rand_selected_colors = torch.unsqueeze((torch.squeeze(self.centers[rand_idxs])).permute(2, 0, 1), 0)
         #self.pixelArtImg = torch.nn.Parameter(torch.clamp(torch.randn(N, C, H, W), min=0.0, max=1.0), requires_grad=True)
         self.pixelArtImg = torch.nn.Parameter(rand_selected_colors, requires_grad=True)
