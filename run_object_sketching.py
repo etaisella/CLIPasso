f35950e09d908e58b9464831cb86fdc342bf3f53
etaisella <74829220+etaisella@users.noreply.github.com>
2022-07-03 13:50:41 +0300

    Update run_object_sketching.py

--- run_object_sketching.py
+++ run_object_sketching.py
@@ -60,7 +60,7 @@ if not os.path.isfile(f"{abs_path}/U2Net_/saved_models/u2net.pth"):
            "-O", "U2Net_/saved_models/"])
 
 test_name = os.path.splitext(args.target_file)[0]
-output_dir = f"{abs_path}/output_sketches/{test_name}_quantColors_{args.numColors}/"
+output_dir = f"{abs_path}/output_sketches/{test_name}_h_{args.canvasH}_w_{args.canvasW}_quantColors_{args.quantizeColors}_{args.numColors}/"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
