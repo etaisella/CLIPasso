f271e8e0400c0a46017036a2d7a6ce1b3210bc6e
etaisella <74829220+etaisella@users.noreply.github.com>
2022-07-03 15:33:49 +0300

    Update run_object_sketching.py

--- run_object_sketching.py
+++ run_object_sketching.py
@@ -94,6 +94,8 @@ losses_all = manager.dict()
 
 
 def run(seed, wandb_name):
+    print("Num Colors")
+    prints(args.numColors)
     exit_code = sp.run(["python", "painterly_rendering.py", target,
                             "--num_paths", str(args.num_strokes),
                             "--output_dir", output_dir,
