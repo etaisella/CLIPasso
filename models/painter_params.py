5e480d6565bf3dbb6d6d81c9ac9266e479d4e0fa
etaisella <74829220+etaisella@users.noreply.github.com>
2022-07-23 19:54:29 +0300

    Update painter_params.py

--- models/painter_params.py
+++ models/painter_params.py
@@ -486,10 +486,10 @@ class PainterOptimizer:
     def update_lr(self, counter):
         if (counter + 1 % 200) == 0:
                self.scheduler.step()
-        if self.args.lr_scheduler:
-            new_lr = self.scheduler.get_lr()
-            for param_group in self.points_optim.param_groups:
-                param_group["lr"] = new_lr
+        #if self.args.lr_scheduler:
+        #    new_lr = self.scheduler.get_lr()
+        #    for param_group in self.points_optim.param_groups:
+        #        param_group["lr"] = new_lr
     
     def zero_grad_(self):
         self.points_optim.zero_grad()
