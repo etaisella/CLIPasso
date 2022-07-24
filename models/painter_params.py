0b6455a961db144d5cb8eb0169447807352d3b29
etaisella <74829220+etaisella@users.noreply.github.com>
2022-07-24 09:47:36 +0300

    Update painter_params.py

--- models/painter_params.py
+++ models/painter_params.py
@@ -486,7 +486,7 @@ class PainterOptimizer:
             self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.points_optim, gamma=0.8)
 
     def update_lr(self, counter):
-        if (((counter + 1) % 300) == 0) and (counter < 1500):
+        if (((counter + 1) % 300) == 0) and (counter < 1000):
             self.scheduler.step()
             print("Sched Step")
             print(self.scheduler.get_lr())
